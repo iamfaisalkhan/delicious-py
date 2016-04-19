1b0311f84763c7d8f555da3cf00e10b5af4e65d5
Faisal Khan <faisal.nust@gmail.com>
2016-04-19 10:52:01 -0500

    Shared array among multi-processes

--- multiprocess/proc.py
+++ multiprocess/proc.py
@@ -2,13 +2,22 @@ import h5py
 import numpy as np
 from multiprocessing import Process, Pool
 
-arr = np.random.rand(10)
+arr = None
+stopAll = true
 
 def f(ar):
 	print id(arr)
-	print arr[0]
 
 if __name__ == '__main__':
+	temp_arr = np.empty((1), np.float32)
+    ctype = np.ctypeslib._typecodes[temp_arr.__array_interface__['typestr']]
+    # create shared ctypes object with no lock
+    shared_obj = mp.RawArray(ctype, 10)
+    
+    # create numpy array from shared object
+    arr = np.frombuffer(shared_obj, dtype)
+    arr = arr.reshape(shape)    
+
 	p = Pool(processes=5)
 	p.map_async(f, [0, 1, 2, 3, 4])
 	p.close()
